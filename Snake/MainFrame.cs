ee8b8920a0a5c5c38ef6996514728cae6462420d
Anders <>
2014-11-18 13:40:30 +0100

    MainLoop

--- Snake/MainFrame.cs
+++ Snake/MainFrame.cs
@@ -1,11 +1,37 @@
 ﻿using System;
-
+using System.Windows;
 namespace Snake
 {
 	public class MainFrame
 	{
+		bool gameIsRunning;
+
 		public MainFrame ()
 		{
+		}
+
+		private void gameLoop(){
+			while(gameIsRunning){
+				updateGameData ();
+				renderData ();
+				putToScreen ();
+			}
+
+		}
+
+		void updateGameData ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		void renderData ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		void putToScreen ()
+		{
+			throw new NotImplementedException ();
 		}
 	}
 }
