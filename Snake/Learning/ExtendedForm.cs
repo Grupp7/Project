4b28ad8624f38137f63da2f66fef5099f557fa11
fikaFaila <anders.angren@hotmail.com>
2014-11-20 00:06:39 +0100

    Control matrix

--- Snake/Learning/ExtendedForm.cs
+++ Snake/Learning/ExtendedForm.cs
@@ -11,6 +11,8 @@ namespace Snake{
 		private IContainer components;
 		private Bitmap backBuffer;
 		private int angle;
+		private float posX = 0;
+		private float posY = 0;
 
 		public ExtendedForm(){
 			this.Show();
@@ -20,6 +22,28 @@ namespace Snake{
 			this.frameRateTimer = new System.Windows.Forms.Timer(this.components);
 			this.frameRateTimer.Enabled = true;
 			this.frameRateTimer.Tick += new System.EventHandler(this.rotateAngle);
+			this.KeyPress +=	new KeyPressEventHandler(this.extendedFormKeyPressed);
+		}
+
+		void extendedFormKeyPressed (object sender, KeyPressEventArgs e){
+
+			
+			switch(e.KeyChar){
+			case (char)Keys.W:
+				posY -= 10;
+				break;
+			case (char)Keys.S:
+				posY += 10;
+				break;
+			case (char)Keys.A:
+				posX -= 10;
+				break;
+			case (char)Keys.D:
+				posX += 10;
+				e.Handled = true;
+				break;
+			}
+
 		}
 
 		private void rotateAngle (object sender, System.EventArgs e){
@@ -29,8 +53,8 @@ namespace Snake{
 			}
 			Invalidate();
 		}
-			
-		private void updateBuffer(){
+
+		private void updateBuffer (){
 			if(backBuffer==null){
 				backBuffer = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
 			}
@@ -61,14 +85,24 @@ namespace Snake{
 			g.Transform = mx;
 			g.FillRectangle(Brushes.Blue, -50, -50, 100, 100);
 
+			//Matrix 3
+			mx = new Matrix();
+			mx.Rotate((-angle * 4), MatrixOrder.Append);
+			float temp = this.ClientSize.Width / 2 + posX;
+			float temp2 = this.ClientSize.Height / 2 + posY;
+			mx.Translate(temp, temp2, MatrixOrder.Append);
+			g.Transform = mx;
+			g.FillRectangle(Brushes.PowderBlue, -50, -50, 100, 100);
+
 			g.Dispose();
 
 		}
+
 		protected override void OnPaint (PaintEventArgs e){
 			updateBuffer();
 			//Move/copy the backBuffer off to the screen
 			//"The Bitmap" object we have painted on
-			e.Graphics.DrawImageUnscaled(backBuffer,0, 0);
+			e.Graphics.DrawImageUnscaled(backBuffer, 0, 0);
 		}
 		// No background paint
 		protected override void OnPaintBackground (PaintEventArgs pevent){
@@ -81,6 +115,8 @@ namespace Snake{
 			}
 			base.OnSizeChanged(e);
 		}
+
+
 	}
 }
 
