e38f905bf9197df4493ec75043eafc4a18f67bb2
fikaFaila <anders.angren@hotmail.com>
2014-11-20 23:49:23 +0100

    Sprite stubs

---
+++ Snake/GameData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Drawing;
+
+namespace Snake
+{
+	class GameData
+	{
+	}
+
+}
+
---
+++ Snake/IGameObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace Snake{
+	public interface IGameObject{
+
+		void passData(GameData newInfo);
+		void update(double gameTime);
+		void draw(Graphics brush);
+		Rectangle getRectangle();
+	}
+}
+
