a1860fa4770b8910ce45221eb9d26edc1917aa8a
Telluscat <telluscat@gmail.com>
2014-12-02 13:47:06 +0100

    k

---
+++ Snake/GameObjects/ShowTimeObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Snake
+{
+	public class ShowTimeObject:IGameObject
+	{
+		private Rectangle location;
+		private Stopwatch stopwatch = new Stopwatch ();
+		private string elaptime;
+
+		public ShowTimeObject ()
+		{
+			this.location = location;
+			stopwatch.Start();
+
+
+		}
+
+		#region IGameObject implementation
+		public void passData (GameData newInfo)
+		{
+
+		}
+		public void update (double gameTime)
+		{
+			stopwatch.Elapsed.ToString(elaptime);
+		}
+		public void draw (System.Drawing.Graphics brush)
+		{
+			// Create string to draw.
+			String drawString = "Score "+ elaptime;
+
+			// Create font and brush.
+			Font drawFont = new Font("Arial", 14);
+			SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+			// Create point for upper-left corner of drawing.
+			PointF drawPoint = new PointF(20.0F, 0F);
+
+			// Draw string to screen.
+			brush.DrawString(drawString, drawFont, drawBrush, drawPoint);
+		}
+		public System.Drawing.Rectangle getRectangle ()
+		{
+			return location;
+		}
+		public bool isColliding (IGameObject objectToTest)
+		{
+			return false;
+		}
+		#endregion
+
+
+	}
+
+
+}
+
