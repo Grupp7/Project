1b52458836f1f27e82d5e4ac368a7078e7e645cc
fikaFaila <anders.angren@hotmail.com>
2014-12-07 18:26:16 +0100

    Grupp 7

--- Snake/Properties/AssemblyInfo.cs
+++ Snake/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Anders")]
+[assembly: AssemblyCopyright ("Grupp 7")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
